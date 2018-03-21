d79916fd38b12fd5de8b27e95e46bd70bacbb9fe
Sean MacDonald <sean@gmail.com>
2018-03-20 19:20:15 -0700

    modified development environment to include firebase information such as the api-key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDC3UTkqRUF2guoIoGVmEbIzELkO_d6i3M",
+    authDomain: "findmysmartpac-1521053573657.firebaseapp.com",
+    databaseURL: "https://findmysmartpac-1521053573657.firebaseio.com",
+    projectId: "findmysmartpac-1521053573657",
+    storageBucket: "findmysmartpac-1521053573657.appspot.com",
+    messagingSenderId: "926755614150"
+  }
 };
